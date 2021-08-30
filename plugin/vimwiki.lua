3fbfee125ed8abad697339491d8608020d3eedc4
raziman <raziman.terra@gmail.com>
2021-08-30 12:42:11 +0800

    add plugin/vimwiki.lua

---
+++ plugin/vimwiki.lua
@@ -0,0 +1,14 @@
+
+
+vim.g.vimwiki_list = {
+  {
+    path = "~/.local/share/nvim/vimwiki",
+    path_html = "~/.local/share/nvim/vimwiki/html",
+  }
+}
+
+vim.g.vimwiki_hl_headers = 1
+vim.g.vimwiki_hl_cb_checked = 1
+
+-- remove tab delay
+vim.g.vimwiki_key_mappings = { table_mappings = 0 }
