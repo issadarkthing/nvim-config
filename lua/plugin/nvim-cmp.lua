b39631f7d10aad5427aa9c473fcf562c0f735c95
raziman <raziman.terra@gmail.com>
2021-08-30 12:40:47 +0800

    add nvim-cmp.lua

---
+++ lua/plugin/nvim-cmp.lua
@@ -0,0 +1,32 @@
+
+local M = {}
+
+function M.setup()
+  local cmp = require('cmp')
+  cmp.register_source('look', require'plugin/cmp_look'.new())
+  cmp.setup {
+    -- You can set mapping if you want.
+    mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<Tab>'] = cmp.mapping.select_next_item(),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      })
+    },
+
+    -- You should specify your *installed* sources.
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'buffer' },
+    },
+  }
+end
+
+return M
