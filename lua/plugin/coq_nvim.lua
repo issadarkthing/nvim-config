320758a67afe3743fb52d9aa0b93b50dc9274eb2
raziman <raziman.terra@gmail.com>
2021-08-30 12:39:32 +0800

    update coq_nvim.lua

--- lua/plugin/coq_nvim.lua
+++ lua/plugin/coq_nvim.lua
@@ -3,11 +3,11 @@ local M = {}
 
 function M.setup()
   vim.g.coq_settings = {
-    match = {
-      max_results = 2000
-    }
+    ["match.max_results"] = 1000,
+    ["clients.snippets.enabled"] = false,
   }
 
+  vim.cmd[[COQnow -s]]
 end
 
 return M
