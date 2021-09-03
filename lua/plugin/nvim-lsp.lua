993a8bd8c7e9682be4174952bb930d1a0b9a46b4
raziman <raziman.terra@gmail.com>
2021-09-03 11:14:42 +0800

    add errors to quickfix list

--- lua/plugin/nvim-lsp.lua
+++ lua/plugin/nvim-lsp.lua
@@ -8,7 +8,6 @@ function M.setup()
   local lspconfig = require('lspconfig')
 
   lspconfig.gopls.setup{}
-  lspconfig.tsserver.setup{}
   lspconfig.diagnosticls.setup{
     filetypes = { "typescript", "typescriptreact", "bash", "sh" },
     init_options = {
@@ -149,6 +148,27 @@ function M.setup()
   "LSP",
   "lsp_show_doc",
   "LSP show documentation")
+
+
+  do
+    local method = "textDocument/publishDiagnostics"
+    local default_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, method, result, client_id, bufnr, config)
+      default_handler(err, method, result, client_id, bufnr, config)
+      local diagnostics = vim.lsp.diagnostic.get_all()
+      local qflist = {}
+      for bufnr, diagnostic in pairs(diagnostics) do
+        for _, d in ipairs(diagnostic) do
+          d.bufnr = bufnr
+          d.lnum = d.range.start.line + 1
+          d.col = d.range.start.character + 1
+          d.text = d.message
+          table.insert(qflist, d)
+        end
+      end
+      vim.lsp.util.set_qflist(qflist)
+    end
+  end
 
 end
 
