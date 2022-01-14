c06cab222e62f6b2439e1def8d20acaca494956b
raziman <raziman.terra@gmail.com>
2022-01-14 22:39:53 +0800

    remove table keymapping

--- plugin/vimwiki.lua
+++ plugin/vimwiki.lua
@@ -11,4 +11,7 @@ vim.g.vimwiki_hl_headers = 1
 vim.g.vimwiki_hl_cb_checked = 1
 
 -- remove tab delay
-vim.g.vimwiki_key_mappings = { table_mappings = 0 }
+vim.g.vimwiki_key_mappings = { 
+  table_mappings = 0,
+  table_format = 0,
+}
